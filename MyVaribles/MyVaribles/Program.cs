b69d2c8b668d736fdb76a6af769bfa0987d891f3
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-11-25 00:56:48 +0100

    First variable
    This is my first variable declaration

---
+++ MyVaribles/MyVaribles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVaribles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myvalue=7;
+
+            Console.Write("My Variable Value = {0}", myvalue);
+            Console.ReadKey();
+        }
+    }
+}
