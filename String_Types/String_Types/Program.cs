0cac8e5f5328a25c29992ea1f98a91d93cff09a7
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-11-25 19:45:26 +0100

    String Types
    This is a course about string types in C#

---
+++ String_Types/String_Types/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //\' \" \\ \0 \a \b \n \r \t \v \f
+            //@
+
+            string myString = "I'm Moncef Oussaf , and this is me not you";
+            string myString2 = @"F:\LocalRepository\CSharpCourseCodes";
+            string myString3 = myString + myString2;
+
+
+            Console.WriteLine("{0}", myString3);
+            Console.ReadKey();
+
+
+        }
+    }
+}
