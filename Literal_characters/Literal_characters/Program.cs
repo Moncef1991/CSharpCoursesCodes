5b3786e59c22be36761abb816c25fd5f995cb958
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-11-25 19:10:04 +0100

    Literal characters
    This is a course of Literal characters in C#

---
+++ Literal_characters/Literal_characters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literal_characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = -100;
+            uint myUVar = 1000;
+            ulong myLVar = 1000;
+
+            float myFLVar = 1.5F;
+            double myDVar = 100.5D;
+            decimal myDCVar = 100.5M;
+
+            Console.WriteLine("{0} {1}", myVar, myUVar);
+            Console.WriteLine($"{myDCVar}, {myDVar}");
+            Console.ReadKey();
+
+        }
+    }
+}
