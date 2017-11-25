6913b72f463b79071a3ae3ffd30e0b62b2baf756
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-11-25 20:10:34 +0100

    First exercise
    This is my first exercise in C#

--- MyExercise1/MyExercise1/Properties/AssemblyInfo.cs
+++ MyExercise1/MyExercise1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyEcxercice1")]
+[assembly: AssemblyTitle("MyExercise1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyEcxercice1")]
+[assembly: AssemblyProduct("MyExercise1")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5c6418f0-f218-443b-a5ca-d39eaf204ace")]
+[assembly: Guid("4de5e696-2377-4f7f-b5ff-0a70277e915f")]
 
 // Version information for an assembly consists of the following four values:
 //
