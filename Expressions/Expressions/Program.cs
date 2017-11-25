cfea3dd0e2243cc9f7e88d8db443186077ba5980
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-11-25 20:48:23 +0100

    Expressions
    This is expressions in C#

---
+++ Expressions/Expressions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myVar = 10.5F;
+            float myVar2 = 10.7F;
+
+            float temp = myVar + myVar2;
+
+            //basic operations + -* /
+            //bitwise operations
+            //logical boolean true , false
+
+            //1- unary operand
+            //2- binary operands
+            //3- 3 operands
+
+        }
+    }
+}
