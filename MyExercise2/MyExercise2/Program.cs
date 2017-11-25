a77d6f9db15d1598f33246e64d53a2e842f1297d
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-11-25 20:12:43 +0100

    Second exercise
    This is my second exercise in C#

---
+++ MyExercise2/MyExercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExercice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Moncef has said \"this is unique course\", and i think so";
+            string myString2 = @"Moncef has said ""this is unique course"", and i think so";
+
+
+            Console.WriteLine("{0}\n{1}", myString, myString2);
+            Console.ReadKey();
+        }
+    }
+}
