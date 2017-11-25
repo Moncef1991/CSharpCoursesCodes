5407a1d920314184e83a6f2f8b28331a071958be
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-11-25 02:18:18 +0100

    First excercice
    This is my first exercice in C#

---
+++ MyEcxercice1/MyEcxercice1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEcxercice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myCharValue1 = 'A';
+            Console.WriteLine("The character value of A is = {0}", myCharValue1);
+
+            int myCharValue2 = 'a';
+            Console.WriteLine("The character value of a is = {0}", myCharValue2);
+
+            string myString1 = "This is my character A value";
+            Console.WriteLine("{0}", myString1);
+
+            string myString2 = "This is my character a value";
+            Console.WriteLine("{0}", myString2);
+
+            Console.WriteLine("{0} {1} and {2} {3}", myString1, myCharValue1, myString2, myCharValue2);
+            Console.ReadKey();
+        }
+    }
+}
