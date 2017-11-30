dd19d57a4f06678d411578f7a476c4c057d934da
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-11-30 17:13:24 +0100

    Assignement Operations
    This is an assignement operations in C#

---
+++ Assignment-Operations/Assignment-Operations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //=, +=, *=, /=, %=,
+            int myVar1 = 7, myVar2 = 3;
+
+            //myVar2 = myVar1;
+
+            //myVar1 += myVar2;
+
+            //myVar1 -= myVar2;
+
+            //myVar1 *= myVar2;
+
+            //myVar1 /= myVar2;
+
+            myVar1 %= myVar2;
+
+
+
+            Console.WriteLine("The result is {0}\nThe second result is {1}", myVar1, myVar2);
+            Console.ReadKey();
+        }
+    }
+}
