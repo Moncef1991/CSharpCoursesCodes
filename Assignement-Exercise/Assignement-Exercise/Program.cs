7d21b24f4221d6ef68b851cebce72d833e1dfb3e
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-11-30 17:24:29 +0100

    Assignement Exercise
    This is a challenge in assignement operations in C#

---
+++ Assignement-Exercise/Assignement-Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 7, myVar2 = 2;
+
+            myVar1 /= ++myVar2;
+
+            Console.WriteLine("The result is {0}\nThe second is {1}", myVar1, myVar2);
+            Console.ReadKey();
+        }
+    }
+}
