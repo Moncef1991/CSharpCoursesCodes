47e4aca1f8c390b84d863c168ef1aaab7dfde5a5
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-01 22:37:10 +0100

    If Exercise
    This is a challenge about 'if' and its conditions in C#

---
+++ If-Challenge/If-Challenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 8;
+            if (myVar > 10)
+                Console.WriteLine("My variable is greater than 10");
+            else
+                Console.WriteLine("My variable is less than 10");
+
+            int myVar1 = 10, myVar2 = 5, myVar3 = 9;
+            if ((myVar1 <= 11) || (myVar2 == 6) && (myVar3 >= 8))
+                Console.WriteLine("\nTrue");
+            else
+                Console.WriteLine("\nFalse");
+
+            Console.ReadKey();
+        }
+    }
+}
