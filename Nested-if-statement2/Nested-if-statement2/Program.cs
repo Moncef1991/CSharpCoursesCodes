66b76365d164e52d622d895ed4f1eb7107326ad4
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-01 23:20:30 +0100

    Nested if statement 2
    This is a second course in nested 'if' statement in C#

---
+++ Nested-if-statement2/Nested-if-statement2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_if_statement2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 4, myVar2 = 6;
+            if(myVar1 <= 3 && myVar2 >=6)
+            {
+                Console.WriteLine("Condition one right");
+            }
+            else if(myVar1 <=4 && myVar2 >= 6)
+            {
+                Console.WriteLine("Condition two right");
+            }
+            else if (myVar2 >=5)
+            {
+                Console.WriteLine("Condition three right");
+            }
+            else
+            {
+                Console.WriteLine("All the conditions are not right");
+            }
+            Console.ReadKey();
+        }
+    }
+}
