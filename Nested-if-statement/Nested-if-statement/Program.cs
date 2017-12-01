5d4dc8fc26d50220789c8e4ac950920344652b61
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-01 22:51:44 +0100

    Nested if statement
    This is a nested if statement course in C#

---
+++ Nested-if-statement/Nested-if-statement/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_if_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 9, myVar2 = 6;
+            if(myVar1 == 10)
+            {
+                Console.WriteLine("myVar1 == 9");
+                if(myVar2 == 7)
+                {
+                    Console.WriteLine("myVar2 == 6");
+                }
+                else
+                {
+                    Console.WriteLine("myVar2 is not == 7");
+                }
+            }
+            else
+            {
+                Console.WriteLine("myVar1 is not == 9");
+            }
+            Console.ReadKey();
+        }
+    }
+}
