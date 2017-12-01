e66364edb592fcfadf63042c6c2226f80bce4514
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-01 21:56:35 +0100

    Boolean Operators
    This is a course about boolean operators in C#

---
+++ Boolean-Operators/Boolean-Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boolean comparaison ==, !=, <, >, <=, >=
+            //Boolean conditions &&, ||
+
+            bool myResult;
+            int myVar1 = 5, myVar2 = 8;
+            myResult = myVar1 <= 6;
+
+            myResult = (myVar1 == 6) || (myVar2 == 8);
+
+            Console.WriteLine("My result is = {0}", myResult);
+            Console.ReadKey();
+        }
+    }
+}
