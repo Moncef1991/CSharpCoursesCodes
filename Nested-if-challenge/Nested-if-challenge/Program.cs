ed22b0e442be202091801badb1a13eb2316d4ba1
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-01 23:03:30 +0100

    Nested if challenge
    This is a challenge in C# about nested 'if' statement

---
+++ Nested-if-challenge/Nested-if-challenge/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_if_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 10, myVar2 = 6, myVar3 = 8;
+
+            if (myVar1 == 10)
+            {
+                Console.WriteLine("myVar1 = 10");
+                if((myVar2>=5)&&(myVar3<=9))
+                {
+                    Console.WriteLine("This is True");
+                }
+                else
+                {
+                    Console.WriteLine("This is False");
+                }
+            }
+            
+            else
+            {
+                Console.WriteLine("myVar1 is not = 10");
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
