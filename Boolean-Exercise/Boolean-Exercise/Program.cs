f89b423009b294e7aef0fff77406379e083f9988
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-01 22:07:39 +0100

    Boolean challenge
    This is a challenge about boolean in C#

---
+++ Boolean-Exercise/Boolean-Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 9, myVar2 = 11;
+            bool myResult1 , myResult2;
+
+            myResult1 = (myVar1 <10) && (myVar2<=11);
+            myResult2 = (myVar1 < 10) || (myVar2 <= 11);
+
+            Console.WriteLine("My result 1 is = {0}\nMy result 2 is = {1}", myResult1, myResult2);
+            Console.ReadKey();
+
+        }
+    }
+}
