68761c85c49f8617c7af1c61b213cfb906a9bddd
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-01 22:25:19 +0100

    If statements
    This is a course about if and the conditions in C#

---
+++ If-Statement/If-Statement/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 11;
+            if (myVar == 11)
+            {
+                myVar = 11;
+                Console.WriteLine("{0}", myVar);
+                Console.WriteLine("My variable = 10");
+            }
+            else
+                Console.WriteLine("My variable is not = 10");
+
+
+            int myVar1 = 9, myVar2 = 7;
+
+            if((myVar1<=7) || (myVar2>=10))
+            {
+                Console.WriteLine("\nMy conditions is true");
+
+            }
+            else
+            {
+                Console.WriteLine("\nMy conditions is false");
+
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
