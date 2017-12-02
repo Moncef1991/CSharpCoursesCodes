6f3ecfd45da0e45d7a56c903f2558af21f931ee4
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-02 21:00:41 +0100

    Switch statement challenge
    This is a switch challenge in C#

---
+++ Switch-challenge/Switch-challenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string whathesay = "Hello";
+
+            switch (whathesay)
+            {
+                case "hello":
+                    Console.WriteLine("He say hello");
+                    break;
+                case "Bye":
+                    Console.WriteLine("He say bye");
+                    break;
+                case "welcome":
+                    Console.WriteLine("He say welcome");
+                    break;
+                default:
+                    Console.WriteLine("No one");
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
