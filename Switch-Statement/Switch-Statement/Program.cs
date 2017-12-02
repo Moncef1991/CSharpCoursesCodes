3f1d4c89ed099744f59d783889e7780335b02a66
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-02 20:49:58 +0100

    Switch statement
    This is a switch statement course in C#

---
+++ Switch-Statement/Switch-Statement/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myAminal = "Cat";
+
+            switch (myAminal)
+            {
+                case "Canary":
+                    Console.WriteLine("My animal is the canary bird");
+                    break;
+                case "Cat":
+                    Console.WriteLine("My animal is a cat");
+                    break;
+                case "Camel":
+                    Console.WriteLine("My animal is a camel");
+                    break;
+                default:
+                    Console.WriteLine("No one");
+                    break;
+            }
+            int myVar = 15;
+            switch (myVar)
+            {
+                case 10:
+                    Console.WriteLine("My variable number value is 10");
+                    break;
+                case 7:
+                    Console.WriteLine("My variable number value is 7");
+                    break;
+                case 4:
+                    Console.WriteLine("My variable number value is 4");
+                    break;
+                default:
+                    Console.WriteLine("No one");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
