9fef7b00ba880ff33df6842d28f419ab81a15de3
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-02 21:12:40 +0100

    Ternary operator
    This is a ternary operator course in C#

---
+++ TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 9;
+
+            string myResult = (myVar < 10) ? "less than 10" : "greater than 10";
+            Console.WriteLine("My variable result is {0}", myResult);
+
+            string myName = "Moncef", result;
+            result = (myName.Equals("Moncef")) ? "Yes" : "No";
+            Console.WriteLine("My variable result is {0}", result);
+
+            Console.ReadKey();
+        }
+        
+    }
+}
