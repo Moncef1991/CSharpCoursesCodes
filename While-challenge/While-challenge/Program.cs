8489e85a3d18d33e61f584fb182fd63ca54c2fc9
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-03 21:47:22 +0100

    While challenge
    This is while challenge in C#

---
+++ While-challenge/While-challenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myWeight = 120, i = 0;
+            
+            while (myWeight > 83)
+            {
+                myWeight -= 1;
+                i++;
+            }
+            Console.WriteLine("I need {0} exercises to reach 83 kg", i);
+            Console.ReadKey();
+        }
+    }
+}
