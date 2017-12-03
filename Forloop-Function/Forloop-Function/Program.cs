2f2540c5be6d2f3c451f9ce3eeb5f77a3e6ec8b2
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-03 21:57:16 +0100

    Forloop Function
    This is a forloop function course in C#

---
+++ Forloop-Function/Forloop-Function/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forloop_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mySalary = 1000, total = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                total += mySalary;
+            }
+            Console.WriteLine("My salary for 12 months is {0}", total);
+            Console.ReadKey();
+        }
+    }
+}
