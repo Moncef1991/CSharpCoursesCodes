024ba0ec1647fe1fa50f23ace7caa3c99fcd7b75
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-03 22:18:14 +0100

    Forloop challenge
    This is a forloop function challenge in C#

---
+++ Forloop-challenge/Forloop-challenge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forloop_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float i = 0;
+            for (double mySalary = 1020; mySalary <= 2000; mySalary += 20)
+            {
+                i++;
+            }
+            Console.WriteLine("I need {0} years to reach 2000", i/12);
+
+            int j = 0;
+            for (int myWeight = 120; myWeight > 83; myWeight--)
+            {
+                j++;
+            }
+            Console.WriteLine("\nI need {0} to reach 83 kg", j);
+            Console.ReadKey();
+        }
+    }
+}
