68e84b61f5b94b373a05bd2a8f1912b9f311c9d4
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-03 21:23:35 +0100

    Do while challenge
    This is a challenge of do while in C#

---
+++ Do-While-challenge/Do-While-challenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mySalary = 1020, i = 1.0;
+            
+            do
+            {
+                mySalary += 20;
+                i++;
+            } while (mySalary <= 2000);
+            Console.WriteLine("I need {0} years to reach 2000", i/12);
+            Console.ReadKey();
+        }
+    }
+}
