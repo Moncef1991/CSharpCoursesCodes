dbd2115230e95cd89513a22a6a5ee0f90a392d85
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-06 20:37:38 +0100

    Break loop continue
    This is break loop continue course in C#

---
+++ Break_Loop_Continue/Break_Loop_Continue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Loop_Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Hello Moncef");
+                if (i > 2)
+                    continue;
+
+                for (int j = 0; j <= 4; j++)
+                {
+                    Console.WriteLine("\t Hello World");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
