f58b8dd8b378f174ff9b6b5d5d7044fc0c47cf28
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-07 14:39:16 +0100

    Implicit Conversion
    This is implicit conversion course in C#

---
+++ Implicit-Conversion/Implicit-Conversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long myVar1 = 1000;
+            float myVar2 = myVar1;
+            Console.WriteLine($"{myVar2}");
+
+            char myVar3 = 'G';
+            int myIntvar = myVar3;
+            Console.WriteLine($"\n{myIntvar}");
+
+            Console.ReadKey();
+        }
+    }
+}
