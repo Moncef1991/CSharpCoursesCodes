b14c90f7244a43a58677d427ea70fca758191f84
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-07 15:11:39 +0100

    Explicit Conversion
    This is explicit conversion course in C#

---
+++ Explicit-Conversion/Explicit-Conversion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Type1 = (Type1)Type2                 //Casting
+
+            byte mySbyte;
+            int myInt = 350;
+
+            // mySbyte = myInt;
+
+            mySbyte = unchecked((byte)myInt);
+
+            Console.WriteLine($"My variable int = {myInt}" + "\n");
+            Console.WriteLine($"My byte variable = {mySbyte}");
+            Console.ReadKey();
+        }
+    }
+}
