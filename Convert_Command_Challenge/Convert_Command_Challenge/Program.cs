feb90a9cf6e314e6fc8d1e9ba9b44eb0e2d7b137
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-07 15:48:09 +0100

    Convert Command Challenge
    This is convert command challenge in C#

---
+++ Convert_Command_Challenge/Convert_Command_Challenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Command_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myFloatvar = 45.56f;
+            string myString, mySubString;
+
+            myString = Convert.ToString(myFloatvar);
+            mySubString = myString.Substring(2,3);
+
+            Console.WriteLine($"My float variable is {myFloatvar}");
+            Console.WriteLine($"My string variable is {myString}\nMy substring variable is {mySubString}");
+            Console.ReadKey();
+        }
+    }
+}
