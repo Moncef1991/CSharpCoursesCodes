a2e8e6fa3b5d42b7357a2a9d85f7b3a9ca41bdc5
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-07 15:36:18 +0100

    Convert Command
    This is convert command course in C#

---
+++ Convert_Command/Convert_Command/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myDoublevar = 2500.45d;
+
+            string myStringvar, mySubString;
+
+            //myDoublevar = myDoublevar;                // Implicit Conversion
+
+            myStringvar = Convert.ToString(myDoublevar);
+
+            mySubString = myStringvar.Substring(0, 2);
+
+            Console.WriteLine($"My variabe is {myDoublevar}\n");
+            Console.WriteLine($"My string variable is {myStringvar}\n");
+            Console.WriteLine($"My substring variable is {mySubString}");
+            Console.ReadKey();
+
+        }
+    }
+}
