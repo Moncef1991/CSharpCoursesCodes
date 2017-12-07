c70b21e01b90ff302b786efd5b9bc0351accedcc
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-07 15:21:12 +0100

    Explicit conversion challenge
    This is explicit conversion challenge in C#

---
+++ Explixit_Conversion_Challenge/Explixit_Conversion_Challenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explixit_Conversion_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal myDecvar = 100.999m;
+            int myIntvar;
+            myIntvar = unchecked((int)myDecvar);
+
+            Console.WriteLine($"My Decimal variable = {myDecvar}" + "\n");
+            Console.WriteLine($"My int variable = {myIntvar}");
+            Console.ReadKey();
+        }
+    }
+}
