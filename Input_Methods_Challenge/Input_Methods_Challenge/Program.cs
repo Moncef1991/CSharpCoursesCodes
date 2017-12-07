70be5fd506e6fd7e3d9c8f30e67ade9a16043f67
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-07 18:22:16 +0100

    Input Methods Challenge
    This is input methods challenge in C#

---
+++ Input_Methods_Challenge/Input_Methods_Challenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_Methods_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName, LastName;
+            decimal Salary, TaxRate = 0.07M;
+
+
+            Console.WriteLine("Enter your First Name\n");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("\nEnter your Last Name\n");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("\nEnter your Salary\n");
+            Salary = (Convert.ToDecimal(Console.ReadLine()));
+
+            Console.WriteLine($"\nWelcome {FirstName} {LastName}\n");
+            Console.WriteLine("Your Salary for 12 months is {0} $ ", ((Salary - (TaxRate * Salary)) * 12));
+            Console.WriteLine("\nYou paid Tax Rate for 12 months {0} $", ((TaxRate * Salary) * 12));
+            Console.ReadKey();
+        }
+    }
+}
