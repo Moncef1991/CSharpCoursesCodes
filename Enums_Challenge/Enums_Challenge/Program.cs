177440fe10a4942ca1d3ed18806a544dffa80514
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-09 19:52:11 +0100

    Enums Challenge
    This is enums challenge in C#

---
+++ Enums_Challenge/Enums_Challenge/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_Challenge
+{
+    class Program
+    {
+        enum orientation : sbyte
+        {
+            north = 2,
+            east =3,
+            south = 4,
+            west = 5
+        }
+        static void Main(string[] args)
+        {
+            sbyte orienSbyte;
+            string myOrien;
+            orientation myOrientation = orientation.east;
+            Console.WriteLine($"The direction = {myOrientation}\n");
+            orienSbyte = (sbyte)myOrientation;
+            Console.WriteLine($"My direction index = {orienSbyte}\n");
+            myOrien = Convert.ToString(myOrientation);
+            Console.WriteLine($"Orientation associated with this index = {myOrien}");
+            Console.ReadKey();
+        }
+    }
+}
