66f1eac6d714871be4e41c6f3984ef517733db8e
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-09 19:37:07 +0100

    Enums
    This is enums course in C#

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        enum humanSex : sbyte
+        {
+            male = 1,
+            female = 2
+
+        }
+        static void Main(string[] args)
+        {
+            sbyte humanSbyte;
+            string humanGender;
+            humanSex myHumansex = humanSex.male;
+            Console.WriteLine($"Human sex = {myHumansex}" + "\n");
+            humanSbyte = (sbyte)myHumansex;
+            humanGender = Convert.ToString(myHumansex);
+            Console.WriteLine($"Human Gender index in = {humanSbyte}" + "\n");
+            Console.WriteLine($"Human associated with this index = {humanGender}" + "\n");
+            Console.ReadKey();
+
+        }
+    }
+}
