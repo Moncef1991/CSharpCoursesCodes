59e0d50f5f45262a293f3c88ec28001cdbc7281b
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-11 21:05:08 +0100

    Array ForLoop 2 Challenge
    This is the second challenge of array forloop 2 in C#

---
+++ Array_ForLoop2_Challenge/Array_ForLoop2_Challenge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_ForLoop2_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumbers = { 3, 10, 14, 55, 0, 1, 0, 12 };
+            int maxValue = 0;
+            
+            for (int i = 0; i < 7; i++)
+            {
+                if (maxValue < myNumbers[i])
+                {
+                    maxValue = myNumbers[i];
+                }
+               
+            }
+            Console.WriteLine($"The max value is = {maxValue}");
+            Console.ReadKey();
+             
+        }
+    }
+}
