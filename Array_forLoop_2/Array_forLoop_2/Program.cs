4eda13fdd1e0ef70e08219316087d2b0503c5303
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-11 20:29:47 +0100

    Array ForLoop 2
    This is the second array forloop course in C#

---
+++ Array_forLoop_2/Array_forLoop_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_forLoop_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int myArraySize = 5;
+            int[] myNumbers1 = { 1, 5, 8, 7, 2 };
+            int[] myNumbers2 = new int[myArraySize];
+            int[] myNumbers3 = new int[myArraySize] { 1, 5, 8, 7, 2 };
+
+            for (int i = 0; i < 5; i++)
+            
+                Console.WriteLine($"My number {i + 1} = {myNumbers1[i]}");
+            
+            Console.ReadKey();
+
+        }
+    }
+}
