69e234b55cd571b566f2d9a506b3759f9e34df04
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-12 22:26:31 +0100

    String Manipulation Operations Challenge
    This is manipulation operations challenge in C#

---
+++ String_Manipulation_Operations_Challenge/String_Manipulation_Operations_Challenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Manipulation_Operations_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString1 = "I am smart", myString2 = "He is", myString;
+            myString = myString2 + (myString1.Substring(4));
+
+            char[] myCharArray = myString.ToCharArray();
+
+            foreach (char myCharStringArray in myCharArray)
+            {
+                Console.Write($"{myCharStringArray}" + " ");
+
+            }
+            Console.ReadKey();
+            
+
+
+
+           
+            
+        }
+    }
+}
