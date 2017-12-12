70ae04dd31d303cdd33ccccce88824dd134e6137
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-12 22:00:49 +0100

    String Manipulation Operations
    This is string manipulation operations course in C#

---
+++ String_Manipulation_Operations/String_Manipulation_Operations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Manipulation_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Moncef Oussaf";
+            char[] myCharArray = myString.ToCharArray();
+            Console.WriteLine($"My string length is {myString.Length}\n");
+
+            foreach(char myChar in myCharArray)
+            {
+                Console.WriteLine($"{myChar}");
+            }
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
