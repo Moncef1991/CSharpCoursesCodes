32e7674e0d6b11e83dc4a6968a0bf8d9a2e3bb99
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-12 22:57:43 +0100

    To Upper To Lower
    This is to lower to upper course in C#

---
+++ To_Upper_VS_To_Lower/To_Upper_VS_To_Lower/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Upper_VS_To_Lower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString2, myString4, myString3,myString5, myString1;
+            char[] mytrimChar = { 'h', 'e', 'i', ' ' };
+            Console.WriteLine("Please enter your string");
+            myString1 = Console.ReadLine();
+            myString2 = myString1.ToUpper();
+            myString3 = myString1.ToLower();
+            myString4 = myString1.Trim();
+            myString5 = myString1.Trim(mytrimChar);
+
+            Console.WriteLine($"{myString2}");
+            Console.WriteLine($"{myString3}");
+            Console.WriteLine($"{myString4}");
+            Console.WriteLine($"{myString5}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
