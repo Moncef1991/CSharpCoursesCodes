5f8ebc455db12870c08dce374f042b66cf626e2f
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-12 21:53:02 +0100

    Jagged Array Challenge
    This is jagged array challenge in C#

---
+++ Jagged_Arrays_Challenge/Jagged_Arrays_Challenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jagged_Arrays_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double [][] myArrays = new double [3][];
+
+            myArrays[0] = new double[6] { 1, 5, 8, 9, 6.4, 9 };
+            myArrays[1] = new double[3] { 0, 4, 3 };
+            myArrays[2] = new double[4] { 2, 9, 3, 10 };
+
+            for (int i = 0; i < myArrays.Length; i++)
+            {
+                Console.Write($"Element({i})");
+
+                for (int j=0; j < myArrays[i].Length;j++)
+                {
+                    Console.Write($"{myArrays[i][j]}" + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
