869c4ba3d72e52132d1133118ae9dc8812c757c0
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-12 19:15:36 +0100

    Array of String
    This is array of string course in C#

---
+++ ArrayOfString/ArrayOfString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myFullName = { "Oussaf", "Moncef" };
+            int myArrayLentgth = myFullName.Length;
+
+            for (int i = 0; i < myArrayLentgth; i++)
+            {
+                Console.Write($"{myFullName[i]}" + " ");
+            }
+            Console.WriteLine($"\nMy array length is {myArrayLentgth}");
+            Console.ReadKey();
+        }
+    }
+}
