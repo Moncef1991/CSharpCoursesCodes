01517fb287fad252ac3d6e01da7a6c1925c2eb87
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-13 19:39:09 +0100

    Parameters vs Arguments
    This is parameters vs arguments course in C#

---
+++ ParametersVsArguments/ParametersVsArguments/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersVsArguments
+{
+    class Program
+    {
+        static void addNumbers(int myFirstNumber, int mySecondNumber)
+        {
+            int temp = myFirstNumber + mySecondNumber;
+            Console.WriteLine($"{temp}");
+        }
+
+        static int addNumbers2(int myFirstNumber, int mySecondNumber)
+        {
+            int temp = myFirstNumber + mySecondNumber;
+            return temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int myVar1 = 5, myVar2 = 3, temp;
+            addNumbers(myVar1, myVar2);
+
+            temp = addNumbers2(myVar1, myVar2);
+            Console.WriteLine($"{temp}");
+
+            Console.ReadKey();
+        }
+    }
+}
