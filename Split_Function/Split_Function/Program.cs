e37b2a3f1672ff4f1d94b7e9a2e57bb3c94977e1
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-13 19:04:08 +0100

    Split Function
    This is split function course in C#

---
+++ Split_Function/Split_Function/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString;
+            char[] separator = { ' ' };
+            myString = Console.ReadLine();
+            string[] myNewWords;
+            myNewWords = myString.Split(separator);
+            foreach(string word in myNewWords)
+            {
+                Console.WriteLine($"{word}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
