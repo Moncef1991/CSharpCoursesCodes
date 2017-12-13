06f6b7d8617f5c31d53f593eab5136724864f238
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-13 19:23:59 +0100

    First Function
    This is my first function in C#

---
+++ First_Function/First_Function/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Function
+{
+    class Program
+    {
+        static void printName()
+        {
+            Console.Write("My name is Moncef");
+        }
+
+        static void Main(string[] args)
+        {
+            printName();
+            Console.ReadKey();
+        }
+    }
+}
