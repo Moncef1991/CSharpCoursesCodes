5d4116ddcbb01af409805fa5d0cac2a933d7e700
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-13 18:07:29 +0100

    Trim Function
    This is trim function course in C#

---
+++ Trim_Function/Trim_Function/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trim_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString2,  myString1;
+            char[] mytrimChar = { 'h', 'e', 'i', ' ' };
+
+            Console.WriteLine("Please enter your string");
+            myString1 = Console.ReadLine();
+            myString1 = myString1.ToLower();
+            myString2 = myString1.Trim(mytrimChar);
+            //myString2 = myString1.TrimEnd(mytrimChar);
+            //myString2 = myString1.PadLeft(25, '-');
+            //myString2 = myString1.PadRight(25, '-');
+
+
+            Console.WriteLine($"{myString2}");
+            Console.ReadKey();
+        }
+    }
+}
