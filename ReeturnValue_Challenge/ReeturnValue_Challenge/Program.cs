6652299711cc6bd96e72505ffeff7bd752308274
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-13 20:17:26 +0100

    Return Value Challenge
    This is return value challenge in C#

---
+++ ReeturnValue_Challenge/ReeturnValue_Challenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReeturnValue_Challenge
+{
+    class Program
+    {
+        static double average(double[] grades)
+        { 
+            double som = 0, averageStudent;
+        
+            for(int i = 0;i < grades.Length; i++)
+            {
+                  som += grades[i];
+                  
+            }
+            averageStudent = som / grades.Length;
+        return averageStudent;
+
+        }
+        static void Main(string[] args)
+        {
+            double[] myGrades = { 12, 7, 9, 15, 5.5, 10.75, 17 };
+            double myAverage = average(myGrades);
+            Console.WriteLine($"My average is = {myAverage}");
+            Console.ReadKey();
+        }
+    }
+}
