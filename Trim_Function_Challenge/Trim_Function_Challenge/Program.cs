377a2a4d8dbac36c26365f66c310c3f1a2ca37d1
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-13 18:17:03 +0100

    Trim Function Challenge
    This is trim function challenge course in C#

---
+++ Trim_Function_Challenge/Trim_Function_Challenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trim_Function_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString2, myString3, myString4, myString1 = "Hello my dear";
+
+            myString2 = myString1.PadRight(14, 'e');
+            myString3 = myString2.PadRight(15, 's');
+            myString4 = myString3.PadRight(16, 't');
+
+            Console.WriteLine($"{myString4}");
+            Console.ReadKey();
+
+        }
+    }
+}
