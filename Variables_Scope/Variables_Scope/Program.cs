d11b5f102efea2d054c7adcd10dbcc6ce78bcb86
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-14 22:00:26 +0100

    Variables Scope
    This is variables scope course in C#

---
+++ Variables_Scope/Variables_Scope/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_Scope
+{
+    class Program
+    {
+        static string myString;
+        static void printmyString()
+        {
+           
+
+            string myString = "My string inside th printmyString methode";
+            Console.WriteLine($"{myString}");
+            Console.WriteLine($"{Program.myString}");
+        }
+        static void Main(string[] args)
+        {
+
+            string myString = "My string inside th main methode";
+            Program.myString = "Global myString variable";
+            printmyString();
+
+            Console.WriteLine($"{myString}");
+            Console.WriteLine($"{Program.myString}");
+            Console.ReadKey();
+        }
+    }
+}
