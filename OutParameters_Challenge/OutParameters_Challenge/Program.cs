91617abd4a5f7373a9fdd92b5fda806c593b1698
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-14 21:35:04 +0100

    Out Parameters Challenge
    This is out parameters challenge in C#

---
+++ OutParameters_Challenge/OutParameters_Challenge/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutParameters_Challenge
+{
+    class Program
+    {
+        static double Average(double[] grades, out double gradeAverage)
+        {
+            double som = 0, averageStudent;
+            gradeAverage = 10;
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                som += grades[i];
+            }
+            averageStudent = som / grades.Length;
+            Console.WriteLine($"Your average is = {averageStudent}\n");
+
+            if (gradeAverage >= 10)
+            {
+                Console.WriteLine("You are graduated student");
+            }
+            else
+            {
+                Console.WriteLine("You are drop-out");
+            }
+            return averageStudent;
+        }
+            static void Main(string[] args)
+        {
+            double gradeAverage;
+            double[] myGrades = { 12, 7, 9, 15, 5.5, 10.75, 17 };
+            double myAverage = Average(myGrades, out gradeAverage);
+            
+            Console.ReadKey();
+        }
+    }
+}
