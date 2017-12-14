ad17888638f89b67a93b3cae9465b67a78e6201c
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-14 20:52:14 +0100

    Out Parameters
    This is a course about out parameters in C#

---
+++ Out_Parameters/Out_Parameters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_Parameters
+{
+    class Program
+    {
+        static int Minvalue(int[] myArray2, out int myIndex)
+        {
+            int minVal = myArray2[0];
+            myIndex = 0;
+
+            for(int i=1; i < myArray2.Length; i++)
+            {
+                if(myArray2[i] < minVal)
+                {
+                    minVal = myArray2[i];
+                    myIndex = i;
+                }
+                        
+            }
+            return minVal;
+
+        }
+        static void Main(string[] args)
+        {
+            int myIndex;
+            int[] myArray = { 11, 10, 43, 6, 22, 8, 9, 13, 50, 22 };
+            int minVal = Minvalue(myArray, out myIndex);
+            Console.WriteLine($"The minimum value in my array is {minVal} and the index is {myIndex}");
+            Console.ReadKey();
+        }
+    }
+}
