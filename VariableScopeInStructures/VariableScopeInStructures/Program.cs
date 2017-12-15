68cf7ce0c34ee3d87fe7b6ab2eba3b684a888fe7
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-15 20:34:18 +0100

    Variable Scope in Structures
    This is a course about variable scope in structures in C#

---
+++ VariableScopeInStructures/VariableScopeInStructures/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableScopeInStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, index = 0;
+            for(i = 0;i < 10; i++)
+            {
+                index = i;
+                Console.WriteLine($"{index}");
+
+            }
+            Console.WriteLine($"Last index output in loop : {index}");
+            Console.ReadKey();
+        }
+    }
+}
