1dea6764c56c5735d214adf8162a987fb2b52dfc
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-15 20:45:00 +0100

    Main Methode
    This is main methode course in C#

---
+++ MainMethode/MainMethode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethode
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine($"{args.Length} command line arguments has entered");
+            foreach(string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            Console.ReadKey();
+            return 0;        }
+    }
+}
