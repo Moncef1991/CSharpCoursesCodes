85760bc761e720fbadaf3c33dcce813e83f638fd
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-15 21:07:27 +0100

    Overloading
    This is overloading course in C#

---
+++ Overloading/Overloading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Program
+    {
+        public static int printNumber(int myNumber)
+        {
+            return myNumber;
+        }
+        public static double printNumber(double myNumber)
+        {
+            return myNumber;
+        }
+     
+        static void Main(string[] args)
+        {
+            int myNumber = 9;
+            Console.Write($"{printNumber(myNumber)}");
+            Console.ReadKey();
+            
+        }
+    }
+}
