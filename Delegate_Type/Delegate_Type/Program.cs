dc2b13bf32be06c3300cfafc350723c706cb1041
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-15 21:51:41 +0100

    Delegate Type
    This is delegate type course in C#

---
+++ Delegate_Type/Delegate_Type/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Type
+{
+    delegate int MyNewNumber(int myNumber);
+
+    class Program
+    {
+        static int myXNumber = 3;
+        static int addFunct(int number)
+        {
+            myXNumber += number;
+            return myXNumber;
+        }
+        static int mulFunct(int number)
+        {
+            myXNumber *= number;
+            return myXNumber;
+        }
+        static void Main(string[] args)
+        {
+            MyNewNumber n1 = new MyNewNumber(addFunct);
+            MyNewNumber n2 = new MyNewNumber(mulFunct);
+            n1(4);
+            Console.Write($"{myXNumber}");
+            n2(9);
+            Console.Write($"\n{myXNumber}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
