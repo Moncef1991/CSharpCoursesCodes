a734394dadbdf640aa11499bcc4542081d158a87
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-15 20:55:03 +0100

    Struct Function
    This is struct function course in C#

---
+++ StructFunction/StructFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructFunction
+{
+    class Program
+    {
+        struct FullName
+        {
+            public string myFirstName, myLastName;
+            public string printName() => myFirstName + " " + myLastName;
+        }
+
+        static void Main(string[] args)
+        {
+            FullName myFullName;
+            myFullName.myFirstName = "Moncef";
+            myFullName.myLastName = "Oussaf";
+            //Console.Write($"{myFullName.myFirstName} {myFullName.myLastName}");
+            Console.Write(myFullName.printName());
+            Console.ReadKey();
+        }
+    }
+}
