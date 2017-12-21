cec951d557a730dee6c2bca96a670c2ac0008c39
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-21 16:20:39 +0100

    Assertion Methods
    This is assertion methods course in C#

---
+++ AssertionMethods/AssertionMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Diagnostics;
+
+namespace AssertionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"Enter a value");
+            int i = Convert.ToInt32(ReadLine());
+            if(i >= 100)
+            {
+                WriteLine($"{i}");
+            }
+            else
+            {
+                Debug.WriteLine("We are here");
+                Debug.Assert(i >= 100, "Short", "Long");
+            }
+            WriteLine("Done");
+            ReadKey();
+        }
+    }
+}
