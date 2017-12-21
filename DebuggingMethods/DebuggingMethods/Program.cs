78c8fa7bc901d56914a1afe861e50c0b3d5e9350
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-21 15:48:27 +0100

    Debugging Methods
    This is debugging methods course in C#

---
+++ DebuggingMethods/DebuggingMethods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Diagnostics;
+
+namespace DebuggingMethods
+{
+    class Program
+    {
+        public static int addFunction(int x, int y)
+        {
+            Debug.WriteLine("We started here in the add function");
+            int z = x + y;
+            Debug.WriteLine("We are done here in the add function");
+            return z;
+        }
+        public static int mulFunction(int x, int y)
+        {
+            Debug.WriteLine("We started here in the mult function");
+            int z = x * y;
+            Debug.WriteLine("We are done here in the mult function");
+            Trace.WriteLine("We are done here in the mult function");
+            return z;
+        }
+        public static void writeFunction(int z)
+        {
+            Debug.WriteLine("We are started here in the whrite function");
+            WriteLine($"{z}");
+            Debug.WriteLine("We are done here in the write function");
+        }
+        static void Main(string[] args)
+        {
+            int myNum, myVar = 4, myVar2 = 7;
+
+            myNum = addFunction(myVar, myVar2);
+            writeFunction(myNum);
+
+            myNum = mulFunction(myVar, myVar2);
+            writeFunction(myNum);
+
+            ReadKey();
+        }
+    }
+}
