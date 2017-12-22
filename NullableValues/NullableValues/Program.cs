5772d70b41793907b951ec690a7001ae42619e5d
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-22 23:07:39 +0100

    Nullable Values
    This is nullable values course in C#

---
+++ NullableValues/NullableValues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myVariable = null;
+            int? myNumber = null;
+            double? myDouble = null;
+
+
+            Console.WriteLine($"{myVariable}" + $"\n{myNumber}" + $"\n{myDouble}");
+            Console.ReadKey();
+        }
+    }
+}
