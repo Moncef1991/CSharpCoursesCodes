3e31c6d165fe2f4b6b2bfc0e498a076dae5d0953
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-22 21:44:25 +0100

    Operations On Namespaces
    This is operations on namespaces course in C#

---
+++ OperationsOn/OperationsOn/Program.cs
@@ -0,0 +1,44 @@
+﻿using static System.Console;
+using YB = Brothers.YoungBrothers.MyYoungBrothers;
+using OB = Brothers.OldBrothers.MyOldBrothers;
+
+namespace OperationsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Those are my familly");
+            YB.print();
+            OB.print();
+            ReadKey();
+        }
+    }
+}
+
+namespace Brothers
+{
+    namespace YoungBrothers
+    {
+        class MyYoungBrothers
+        {
+            public static void print()
+            {
+                Write("Mounir " + "and" + " Aiach");
+            }
+        }
+    }
+    namespace OldBrothers
+    {
+        class MyOldBrothers
+        {
+            public static void print()
+            {
+                Write("\nHoussem " + "and" + " Baatach");
+            }
+        }
+    }
+}
+
+
+
