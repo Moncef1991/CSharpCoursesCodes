2fdc22b1ffcbdeb2a15e7b3e38e73eaaf0ce217e
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-22 22:27:09 +0100

    LambdaExpression
    This is lambda expression course in C#

---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LambdaExpression
+{
+    delegate int MyNewNumber(int myNumber);
+    class Program
+    {
+        static int myNumber = 3;
+        static void Main(string[] args)
+        {
+            //MyNewNumber n = x => x + myNumber;
+            //MyNewNumber n1 = x => x * myNumber;
+
+            //Write($"My x number = {n(3)}\n");
+            //Write($"My x number = {n1(4)}\n");
+            //ReadKey();
+
+            Func<int, bool> func = (x => x % 2 != 0);
+            int[] myArray = { 1, 2, 4, 3, 5, 6, 7 };
+            for (int i = 0; i < myArray.Length; i++)
+                WriteLine(func.Invoke(myArray[i]));
+
+            ReadKey();
+        }
+    }
+}
