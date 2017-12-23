035c76439c18e98a372df1591a999c7ad1672b31
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-23 20:21:49 +0100

    Recursive Function
    This is recursive function course in c#

---
+++ RecursiveFunction/RecursiveFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFunction
+{
+    class Program
+    {
+        static int MyRecursiveFunction(int myValue,ref int counter)
+        {
+            counter++;
+            //if value=0
+            if (myValue == 0)
+                return 0;
+            //if value less than 10
+            else if(myValue<10)
+            {
+                return MyRecursiveFunction(myValue + 1, ref counter);
+
+            }
+            //ifvalue equal or exceeds 10
+            return myValue;
+        }
+        static void Main(string[] args)
+        {
+            int counter = 0, myResult;
+            myResult = MyRecursiveFunction(4, ref counter);
+            Console.WriteLine($"My result is {myResult} and counter value is {counter}");
+            Console.ReadKey();
+        }
+    }
+}
