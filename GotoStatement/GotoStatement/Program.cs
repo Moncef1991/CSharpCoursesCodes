aaa8929cb42422e98e7dd353989a3561a1e4c32d
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-23 20:07:23 +0100

    Goto Statement
    This is goto statement course in C#

---
+++ GotoStatement/GotoStatement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace GotoStatement
+{
+    class Program
+    {
+        static int MyFunction()
+        {
+            int counter = 0;
+            for(int i=0;i<10;i++)
+            {
+                for(int j=0;j<10;j++)
+                {
+                    if(j==5)
+                    {
+                        goto Mylebel;
+                    }
+                    counter++;
+                }
+            Mylebel:
+                continue;
+            }
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            //WriteLine(MyFunction());
+
+            int counter = 0;
+        myLable2:
+            if (counter < 10)
+            {
+                counter++;
+                goto myLable2;
+            }
+            else
+                WriteLine("Done");
+            WriteLine(counter);
+
+            ReadKey();
+        }
+    }
+}
