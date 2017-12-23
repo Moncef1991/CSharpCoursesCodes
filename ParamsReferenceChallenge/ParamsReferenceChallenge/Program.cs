d4062f1503ad264c56e324b92971f7a3c54f8aa5
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-23 18:06:34 +0100

    Params Reference Challenge
    This is params reference challenge in C#

---
+++ ParamsReferenceChallenge/ParamsReferenceChallenge/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsReferenceChallenge
+{
+    class Program
+    {
+        static double MaxValue(params double[] values)
+        {
+            Console.WriteLine("You entered {0} values.", values.Length);
+            double max = 0;
+
+            if (values.Length == 0)
+                return max;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] > max)
+                {
+                    max = values[i];
+                }
+            }
+            return max;
+
+        }
+        static void Main(string[] args)
+        {
+            double maxValue;
+            maxValue = MaxValue(58.09, 93.27, 6.67, 1.44, 13);
+            Console.WriteLine("The maximum value is : {0}", maxValue);
+            Console.WriteLine();
+
+            double[] data = { 40.09, 300.2, 445.7,0.98 };
+            maxValue = MaxValue(data);
+            Console.WriteLine("The maximum value is : {0}", maxValue);
+            Console.WriteLine();
+
+            Console.WriteLine("The maximum value is : {0}", MaxValue());
+
+            Console.ReadKey();
+        }
+    }
+}
