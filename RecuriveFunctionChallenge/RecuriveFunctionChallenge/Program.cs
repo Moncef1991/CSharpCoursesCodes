32ebe982a3b8546ad30588bc6115c27a821054a2
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-23 20:48:52 +0100

    Recursive Function Challenge
    This is recursive function challenge in C#

---
+++ RecuriveFunctionChallenge/RecuriveFunctionChallenge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace RecuriveFunctionChallenge
+{
+    class Program
+    {
+        static int FactFunction(int myValue, ref int counter)
+        {
+            counter++;
+            if (myValue == 0)
+                return 1;
+
+            else 
+            {
+                return myValue * FactFunction(myValue - 1, ref counter);
+
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            int counter = 0, myResult;
+            myResult = FactFunction(5, ref counter);
+            Console.WriteLine($"My result is {myResult} and counter value is {counter}");
+            Console.ReadKey();
+        }
+    }
+}
