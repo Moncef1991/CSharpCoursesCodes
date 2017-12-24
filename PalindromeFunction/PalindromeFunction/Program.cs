ca90e25c42c827e81cf7a4031cfe01a8ed2354af
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-24 18:20:38 +0100

    Palindrome Function
    This is palindrome function course in C#

---
+++ PalindromeFunction/PalindromeFunction/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeFunction
+{
+    class Program
+    {
+        public static bool MyPolindromeFunction(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while(true)
+            {
+                if(min>max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+                if(char.ToLower(a) !=char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] myArray = { "civic", "deified", "deleveled", "perls", "Is", "Not", "A", "Palindrome","" };
+            foreach(string word in myArray)
+            {
+                Console.WriteLine("{0}={1}", word, MyPolindromeFunction(word));
+            }
+            Console.ReadKey();
+        }
+    }
+}
