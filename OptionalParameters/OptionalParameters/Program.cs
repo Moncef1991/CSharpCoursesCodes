0cd1ca2d4aa2ddc4adfbf8c3b9ee2460033e9282
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-24 18:06:42 +0100

    Optional Parameters
    This is optional parameters course in C#

---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        public static void PrintValues(string name="Moncef", int myValue=7)
+        {
+            Console.WriteLine($"Name = {name} \nNumber = {myValue}\n");
+        }
+
+        static void Main(string[] args)
+        {
+            string name = "Ahmed";
+            int value = 15;
+            PrintValues();
+            PrintValues(name);
+            PrintValues(name, value);
+            PrintValues(name:"Houssem",myValue:6);
+            Console.ReadKey();
+        }
+    }
+}
